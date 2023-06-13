5f88a1e81deba6c8be8f81ff669bc9fdef1ca433
dohi <masa0u0masa1215@gmail.com>
2023-06-13 17:25:45 +0900

    edit: o3d_visualize_points.py

--- section_basics/o3d_visualize_points.py
+++ section_basics/o3d_visualize_points.py
@@ -5,7 +5,8 @@ import open3d as o3d
 filename = sys.argv[1]
 
 print("Loading a point cloud from", filename)
-pcd = o3d.io.read_point_cloud(filename)
+pcd = o3d.io.read_point_cloud(filename)      # 点群として読み込み
+# pcd = o3d.io.read_triangle_mesh(filename)  # メッシュデータとして読み込み
 
 print(pcd)
 print(np.asarray(pcd.points))
